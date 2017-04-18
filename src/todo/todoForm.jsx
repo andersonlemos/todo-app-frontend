7e4abc1d7a9ab55cda30798609d3a32fed6e208b
andersonlemos <anderson.lemos@dmorj.com.br>
2017-04-18 17:44:55 -0300

    update reducers

--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
+
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
+import {changeDescription} from './todoActions'
 
-export default props =>{
+const TodoForm =  props =>{
       const keyHandler = (e) =>{
             if(e.key==='Enter'){
                   e.shiftKey ? props.handleSearch( ):props.handleAdd()
@@ -15,15 +19,19 @@ export default props =>{
                               <Grid cols='12 9 10'>
                               <input id='description'  className='form-control' 
                                                       placeholder='Adicione uma tarefa'
-                                                       onChange={props.handleChange} 
+                                                       onChange={props.changeDescription} 
                                                        onKeyUp={keyHandler}
-                                                       value={props.description}></input>
+                                                       value={props.description}/>
                               </Grid>
                               <Grid cols='12 3 2'>
-                              <IconButton  style='primary' icon='plus'  onClick={props.handleAdd}/>
+                              <IconButton  style='primary' icon='plus'   onClick={props.handleAdd}/>
                               <IconButton  style='info' icon='search'      onClick={props.handleSearch}/>  
-                              <IconButton  style='default' icon='close'      onClick={props.handleClear}/>  
+                              <IconButton  style='default' icon='close'  onClick={props.handleClear}/>  
                               </Grid>
                         </div>
                   )
 }
+
+const mapStateToProps = state => ({description:state.todo.description})
+const mapDispatchToProps = dispatch => bindActionCreators({changeDescription},dispatch)
+export default connect(mapStateToProps,mapDispatchToProps)(TodoForm)
--- src/todo/todoList.jsx
+++ src/todo/todoList.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import {connect} from 'react-redux'
+
 import IconButton from '../template/iconButton.jsx'
 
-export default props => {
+const TodoList =  props => {
 
         const renderRows = () =>{
                 const list = props.list || []
@@ -33,3 +35,5 @@ export default props => {
         </table>
      )
 }
+const mapStateToProps = state => ({list: state.todo.list})
+export default connect(mapStateToProps)(TodoList)
